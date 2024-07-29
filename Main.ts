3292865d3e86c0869a6a4ced8c73ed715b6445bd
SodaSiz <despairos@proton.me>
2024-07-29 15:05:22 +0200

    Renamed the TOKEN .env variable to DISCORD_TOKEN.

--- Main.ts
+++ Main.ts
@@ -1,9 +1,9 @@
-import { Client, Collection } from 'discord.js-selfbot-v13';
-import type { ClientAttributes } from './Types/Client';
-import 'dotenv/config';
-import logger from './Utils/Logger/Logger';
-import { attachErrorHandlers } from './Utils/Handlers/Errors/Attach_On_Error';
-import { readStartupFile } from './Utils/Misc/ASCII/Startup';
+import { Client, Collection } from "discord.js-selfbot-v13";
+import type { ClientAttributes } from "./Types/Client";
+import "dotenv/config";
+import logger from "./Utils/Logger/Logger";
+import { attachErrorHandlers } from "./Utils/Handlers/Errors/Attach_On_Error";
+import { readStartupFile } from "./Utils/Misc/ASCII/Startup";
 
 // Création du client
 const client = new Client() as ClientAttributes;
@@ -11,36 +11,38 @@ client.commands = new Collection();
 
 // Fonction pour charger les gestionnaires dynamiquement
 const loadHandlers = async (handlers: string[]) => {
-    try {
-        await Promise.all(handlers.map(async (handler) => {
-            const module = await import(`./Utils/Handlers/Bot/${handler}`);
-            if (module.default) {
-                module.default(client);
-            } else {
-                logger.error(`Module ${handler} ne possède pas d'export par défaut`);
-            }
-        }));
-    } catch (error) {
-        logger.error(`Erreur lors du chargement des handlers : ${error}`);
-    }
+  try {
+    await Promise.all(
+      handlers.map(async (handler) => {
+        const module = await import(`./Utils/Handlers/Bot/${handler}`);
+        if (module.default) {
+          module.default(client);
+        } else {
+          logger.error(`Module ${handler} ne possède pas d'export par défaut`);
+        }
+      }),
+    );
+  } catch (error) {
+    logger.error(`Erreur lors du chargement des handlers : ${error}`);
+  }
 };
 
 // Fonction principale pour initialiser le bot
 (async () => {
-    try {
-        // Lire le fichier de démarrage
-        await readStartupFile();
-        
-        // Attacher les gestionnaires d'erreurs
-        attachErrorHandlers();
-        
-        // Charger les gestionnaires
-        await loadHandlers(['Commands', 'Events']);  // Ajoutez ou retirez les noms de fichiers ici
+  try {
+    // Lire le fichier de démarrage
+    await readStartupFile();
 
-        // Connexion du client
-        await client.login(process.env.TOKEN);
-        logger.info('Le client est connecté.');
-    } catch (error) {
-        logger.error(`Erreur lors de l'initialisation du client : ${error}`);
-    }
+    // Attacher les gestionnaires d'erreurs
+    attachErrorHandlers();
+
+    // Charger les gestionnaires
+    await loadHandlers(["Commands", "Events"]); // Ajoutez ou retirez les noms de fichiers ici
+
+    // Connexion du client
+    await client.login(process.env.DISCORD_TOKEN);
+    logger.info("Le client est connecté.");
+  } catch (error) {
+    logger.error(`Erreur lors de l'initialisation du client : ${error}`);
+  }
 })();
