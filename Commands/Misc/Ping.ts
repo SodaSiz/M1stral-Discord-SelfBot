1033e8f112b48a4c0fda49fe91c308821cbe7a5c
SodaSiz <despairos@proton.me>
2024-08-06 01:36:57 +0200

    Added some badass embed to some random commands.

--- Commands/Misc/Ping.ts
+++ Commands/Misc/Ping.ts
@@ -1,10 +1,19 @@
-import { Message } from 'discord.js-selfbot-v13';
-import type {ClientAttributes} from '../../Types/Client';
+import { WebEmbed, type Message } from "discord.js-selfbot-v13";
+import type { ClientAttributes } from "../../Types/Client";
 
 export default {
-  name: 'ping',
-  description: 'Affiche la latence du bot et de l\'API de Discord.',
+  name: "ping",
+  description: "Affiche la latence du bot et de l'API de Discord.",
   run: (client: ClientAttributes, message: Message) => {
-    message.channel.send(`Pong ! :ping_pong::\nLatence du client: \`${client.ws.ping}\`\nLatence du bot: <t:${client.readyTimestamp ? Math.floor(client.readyTimestamp / 1000) : ''}>`);
+    const embed = new WebEmbed()
+      .setTitle("Pong ! 🏓")
+      .setDescription(
+        `Latence du client : ${client.ws.ping}ms${
+          client.readyTimestamp
+            ? `\nLatence du bot: ${Math.floor(client.readyTimestamp / 1000)}`
+            : ""
+        }`
+      );
+    return message.channel.send({ content: `${WebEmbed.hiddenEmbed}${embed}` });
   },
-}
+};
--- Commands/Misc/Send_Token.ts
+++ Commands/Misc/Send_Token.ts
@@ -1,11 +1,15 @@
-import { Message } from "discord.js-selfbot-v13";
+import { WebEmbed, type Message } from "discord.js-selfbot-v13";
 import type { ClientAttributes } from "../../Types/Client";
-import 'dotenv/config';
+import "dotenv/config";
 
 export default {
   name: "sendtoken",
-  description: "Renvoie votre token d'authentification Discord (à garder secret et ne pas envoyer n'importe où !).",
+  description:
+    "Renvoie votre token d'authentification Discord (à garder secret et ne pas envoyer n'importe où !).",
   run: (client: ClientAttributes, message: Message) => {
-    return message.channel.send(process.env.DISCORD_TOKEN as string);
-  }
-}
+    const embed = new WebEmbed()
+      .setTitle("Discord Token")
+      .setDescription(process.env.DISCORD_TOKEN as string);
+    return message.channel.send({ content: `${WebEmbed.hiddenEmbed}${embed}` });
+  },
+};
