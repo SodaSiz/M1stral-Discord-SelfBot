7282a54656cc2b1b6040eed3457cd4c1abca217f
SodaSiz <despairos@proton.me>
2024-08-19 20:13:28 +0200

    Some modification to check if a command exist.

--- Commands/Misc/Help.ts
+++ Commands/Misc/Help.ts
@@ -1,8 +1,7 @@
 import { Message } from "discord.js-selfbot-v13";
 import { ClientAttributes } from "../../Types/Client";
 import { prefix } from "../../user-data/Settings/Bot/Bot.json";
-import { embed_error } from "../../Components/Embeds/Error";
-import { embed } from "../../Components/Embeds/Default";
+import { EmbedBuilder } from "../../Components/Embeds/Builder";
 
 export default {
   name: "help",
@@ -16,38 +15,48 @@ export default {
           acc[cmd.category].push(cmd.name);
           return acc;
         },
-        {}
+        {},
       );
 
       let categories = "";
       for (const [category, commands] of Object.entries(commandFolder)) {
         categories += `\n\n- **${category}**: \`\`\`${commands.join(
-          ", "
+          ", ",
         )}\`\`\`\n`;
       }
 
       // Envoyer toutes les catégories en un seul message
       return message.channel.send(
-        `**Liste des commandes disponibles:**${categories}Pour plus d'informations sur une commande, tapez \`${prefix}help <votre commande>\``
+        `**Liste des commandes disponibles:**${categories}Pour plus d'informations sur une commande, tapez \`${prefix}help <votre commande>\``,
       );
     } else {
-      const cmd = client.commands.get(args[0]);
-
-      if (!cmd)
-        return embed_error(
-          message,
-          `La commande que vous recherchez n'existe pas !\nPour obtenir la liste des commandes, tapez ${prefix}help`
-        );
-      else {
-        return embed(
-          message,
-          `Commande ${cmd.name}`,
-          `- Description de la commande : ${cmd.description}\n${
+      try {
+        const cmd = client.commands.get(args[0]);
+        const embed = new EmbedBuilder.Default({
+          title: `Commande ${cmd.name}`,
+          description: `Description de la commande : ${cmd.description}\n${
             cmd.usage
-              ? `\n- Utilisation de la commande : ${prefix}${cmd.name} ${cmd.usage}`
+              ? `Utilisation de la commande : ${prefix}${cmd.name} ${cmd.usage}`
               : ""
-          }`
-        );
+          }`,
+        });
+
+        // Envoyez le message avec l'embed
+        await message.channel.send({
+          content: embed.toString(),
+        });
+      } catch (error) {
+        if (error instanceof TypeError) {
+          const embed = new EmbedBuilder.Error({
+            title: "Commande introuvable",
+            description: `La commande que vous recherchez n'existe pas !\nPour obtenir la liste des commandes, tapez ${prefix}help`,
+          });
+
+          // Envoyez le message avec l'embed
+          await message.channel.send({
+            content: embed.toString(),
+          });
+        }
       }
     }
   },
